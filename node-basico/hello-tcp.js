f8b234af4eb33ceb3947279e19a64ebdd90c7e07
Daniel Castro <120190089+danielmccastro@users.noreply.github.com>
2023-07-06 16:13:29 -0300

    Finished chap 4 projects

---
+++ node-basico/hello-tcp.js
@@ -0,0 +1,9 @@
+var net = require("net");
+var count = 1;
+var server = net.createServer(function (socket) {
+    console.log("Cliente conectou do IP: " + socket.remoteAddress);
+    socket.end("Hello World TCP: " + (count++) + "\n");
+});
+
+server.listen(3000, "localhost");
+console.log("Servidor TCP iniciado...");
