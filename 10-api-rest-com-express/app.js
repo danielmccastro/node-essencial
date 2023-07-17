ad55211905e14973e6ffc5482cd262a4ae09a982
Daniel Castro <120190089+danielmccastro@users.noreply.github.com>
2023-07-17 17:10:35 -0300

    Created routes

--- 10-api-rest-com-express/app.js
+++ 10-api-rest-com-express/app.js
@@ -1,7 +1,55 @@
 let express = require("express");
 let app = express();
+const CarroDB = require("./CarroDB");
+let bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
 app.get("/", (req, res) => {
 res.send("API dos Carros");
+});
+
+app.get("/carros", (req, res) => {
+    CarroDB.getCarros((carros) => {
+        res.json(carros);
+    })
+});
+
+app.get("/carros/:id(\\d+)", (req, res) => {
+    let id = req.params.id;
+    CarroDB.getCarroById(id, (carro) => {
+        res.json(carro);
+    });
+});
+
+app.delete("/carros/:id(\\d+)", (req, res) => {
+    let id = req.params.id;
+    console.log("Deletar carro " + id);
+    CarroDB.deleteById(id, (affectedRows) => {
+        res.json({msg: "Carro deletado com sucesso."})
+    })
+});
+
+app.get("/carros/:tipo", (req, res) => {
+    let tipo = req.params.tipo;
+    CarroDB.getCarrosByTipo(tipo, (carros) => {
+        res.json(carros);
+    });
+});
+
+app.post("/carros", (req, res) => {
+    let carro = req.body;
+    CarroDB.save(carro, (carro) => {
+        res.json(carro);
+    });
+});
+
+app.put("/carros", (req, res) => {
+    let carro = req.body;
+    CarroDB.update(carro, (carro) => {
+        res.json(carro);
+    });
 });
 
 let server = app.listen(3000, () => {
