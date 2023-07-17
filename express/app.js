a15fdf3950bc8da9d44ee4d7384e9094470b6a36
Daniel Castro <120190089+danielmccastro@users.noreply.github.com>
2023-07-17 16:17:28 -0300

    Updated app.js

--- express/app.js
+++ express/app.js
@@ -1,9 +1,41 @@
 let express = require("express");
 let app = express();
+let bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.get("/", function(req, res) {
-    res.json({nome: 'Ricardo', sobrenome: 'Lecheta'})
+    res.status(500).json({msg: 'Ops, nao foi possivel atender essa requisicao.'})
 });
+
+app.get("/pessoa", function(req, res) {
+    let nome = req.query.nome;
+    let sobrenome = req.query.sobrenome;
+    res.status(200).type("text");
+    res.send(nome + " " + sobrenome);
+});
+
+app.get("/pessoa/:id", function(req, res) {
+    let id = req.params.id;
+    res.send("Buscar a pessoa: " + id);
+});
+
+app.get("/pessoa/nome/:nome/sobrenome/:sobrenome", function(req, res) {
+    let nome = req.params.nome;
+    let sobrenome = req.params.sobrenome;
+    res.send(nome + " " + sobrenome);
+});
+
+app.post("/pessoa", function (req, res) {
+    let nome = req.body.nome;
+    let sobrenome = req.body.sobrenome;
+    res.send(nome + " " + sobrenome);
+    if (req.is("json")) {
+        res.json({nome: nome, sobrenome: sobrenome});
+    } else {
+        res.type("text").send("Texto: " + nome + " " + sobrenome);
+    }
+})
 
 let server = app.listen(3000, function() {
     let host = server.address().address;
